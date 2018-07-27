51c82b869ed984199c1833b15d00fca24caaa869
Meghan Gutshall <meghan.gutshall@gmail.com>
2018-07-27 17:41:14 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,3 @@
 def reverse_each_word(sentence)
-  reversed_sentence = []
-  split_sentence = sentence.split(" ")
-  split_sentence.collect do |word|
-    reversed_sentence << word.reverse
-  end
-  reversed_sentence.join(" ")
+  sentence.split(" ").collect { |word| word.reverse }.join(" ")
 end
